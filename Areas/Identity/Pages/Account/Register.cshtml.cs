c48d512c49b4e882e15d551831ecc44f2c4b1d14
khaled khamees <khaledkhameesabdelaleem@gmail.com>
2024-10-12 17:02:23 +0300

    Just Admin Can Add Company and Employee Accounts

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -197,7 +197,14 @@ namespace MVCProject2.Areas.Identity.Pages.Account
                     }
                     else
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        if (User.IsInRole(SD.Role_Admin))
+                        {
+                            TempData["success"] = "User Add Sussessfully";
+                        }
+                        else
+                        {
+                            await _signInManager.SignInAsync(user, isPersistent: false);
+                        }
                         return LocalRedirect(returnUrl);
                     }
                 }
